73181ebcacf74101985146abea5d7d07d5c6d0d6
zydan25 <zydanalattab25w@gmail.com>
2026-09-22 19:37:18 +0300

    infra: add Flask-Migrate Alembic baseline and migration scaffolding

---
+++ migrations/env.py
@@ -0,0 +1,40 @@
+from logging.config import fileConfig
+
+from alembic import context
+from flask import current_app
+
+from app import create_app
+from app.extensions import db
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+app = create_app()
+with app.app_context():
+    target_metadata = db.metadata
+
+
+def get_url():
+    return current_app.config["SQLALCHEMY_DATABASE_URI"]
+
+
+def run_migrations_offline():
+    context.configure(url=get_url(), target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"}, compare_type=True, compare_server_default=True)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = db.engine
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True, compare_server_default=True)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
