c3421527f7fa6a333836bcf15e362385733bd469
zydan25 <zydanalattab25w@gmail.com>
2026-09-22 18:04:18 +0300

    security: scope realtime events and protect state-changing requests

--- app/extensions.py
+++ app/extensions.py
@@ -2,10 +2,12 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
-socketio = SocketIO(async_mode="threading", cors_allowed_origins="*")
+csrf = CSRFProtect()
+socketio = SocketIO(async_mode="threading")
